2e5132d64d7b93b59a349b2a216f235a7755637c
Beatriz <beatrizrmb@outlook.com>
2023-09-12 10:58:16 +0100

    popcorn-time initialization

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer(){
+    return <h1>This component is the footer</h1>
+}
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+function Main() {
+    return <h1>This component is the Main</h1>
+}
+
+export default Main;
